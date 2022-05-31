34774ba9d0459707204333f7df04c52392bf21f3
AlexEgupov <alex.egupov7@gmail.com>
2022-05-31 14:37:00 +0300

    fix script.js lesson04

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 'use strict';
 
-let letters = prompt('Введите больше или меньше 30 символов', 'Надо дописать еще немного');
 let arr = [];
+let letters = prompt('Введите больше или меньше 30 символов', 'Надо дописать еще немного');
 
 function myfunc(str) {
     if (typeof str !== 'string') {
-        alert('Не строка');
+        return 'Не строка';
     } else {
         let strCut = str.trim();
         if (strCut.length > 30) {
             strCut = strCut.substring(0, 30) + '...';
             return strCut;
         } else {
-            return strCut;
+            return 'Попробуйте ввести больше символов';
         }
     };
 };
 
-alert(myfunc(arr));
-alert(myfunc(letters));
 
+alert(myfunc(letters));
+console.log(myfunc(arr));
