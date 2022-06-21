b2f0d41f70691a62a66ecb35fa71e8356bbb229e
AlexEgupov <alex.egupov7@gmail.com>
2022-06-22 01:01:34 +0300

    feat script.js lesson19plus 2

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-// 'use strict';
+'use strict';
 
-const par = document.querySelector('p');
-const input = document.querySelector('input');
+const body = document.querySelector('body');
+let active = false;
+let buttonReset;
+let buttonStart;
+let div;
+let count = 0;
+let idInterval;
 
-function debounce(func, time) {
-    return function () {
-        let previousCall = this.lastCall;
-        this.lastCall = Date.now();
-        if (previousCall && this.lastCall - previousCall <= time) {
-            clearTimeout(this.lastCallTimer);
-        }
-        this.lastCallTimer = setTimeout(() => func(), time);
-    };
-}
+body.insertAdjacentHTML('beforeend',
+    `<div style='height: 100px; width: 100px; background: blue; position: absolute;'></div>`);
 
-let printFunc = () => {
-    par.textContent = input.value;
+body.insertAdjacentHTML('beforeend',
+    `<button class='reset' style='  padding: 15px;background: #red ;position: fixed; top: 50%; left: 55%; transform: translate(-50%, -50%);'>Reset</button>`);
+
+body.insertAdjacentHTML('beforeend',
+    `<button class='start' style='  padding: 15px;background: #red ;position: fixed; top: 50%; left: 45%; transform: translate(-50%, -50%);'>Start/Stop</button>`);
+
+div = document.querySelector('div');
+buttonReset = document.querySelector('.reset');
+buttonStart = document.querySelector('.start');
+
+const flySquare = () => {
+    count++;
+    idInterval = requestAnimationFrame(flySquare);
+
+    if (count < 100) {
+        div.style.left = count * 5 + 'px';
+    } else if (count < 200) {
+        div.style.left = count * 5 + 'px';
+    } else {
+        cancelAnimationFrame(idInterval);
+    }
 };
 
-let debouncedPrint = debounce(printFunc, 300);
+buttonStart.addEventListener('click', () => {
+    if (active) {
+        cancelAnimationFrame(idInterval);
+        active = false;
+        console.log(2);
+    } else {
+        idInterval = requestAnimationFrame(flySquare);
+        active = true;
+        console.log(1);
+    }
+});
 
-input.addEventListener('input', () => {
-    debouncedPrint();
+buttonReset.addEventListener('click', () => {
+    count = 0;
+    active = false;
+    div.style.left = '8px';
 });
